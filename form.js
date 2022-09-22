d65e2ed712b64872c30f1656458b3487c1f105df
Ibrahim <sir.ibrahim.002@gmail.com>
2022-09-22 16:53:19 -0500

    Modificando ventana modal

--- form.js
+++ form.js
@@ -1,10 +1,12 @@
 let profileButton = document.querySelector('.profile__button');
 let editForm = document.querySelector('.edit-form');
 let buttonClose = document.querySelector('.button-close');
+let profile = document.querySelector('.profile');
 
 profileButton.addEventListener('click', (e)=>{
     e.preventDefault();
     editForm.classList.add('open-form');
+    editForm.classList.add('modal');
 })
 
 buttonClose.addEventListener('click', (e)=>{
