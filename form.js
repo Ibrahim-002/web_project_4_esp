395f507950a01b4f491c9457c06e1b0997f66318
Ibrahim <sir.ibrahim.002@gmail.com>
2022-09-27 22:38:02 -0500

    Modificando interacciones en modal

--- form.js
+++ form.js
@@ -1,15 +1,45 @@
 let profileButton = document.querySelector('.profile__button');
-let editForm = document.querySelector('.edit-form');
-let buttonClose = document.querySelector('.button-close');
-let profile = document.querySelector('.profile');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let popupSave = document.querySelector('.popup__save');
+
+let namedInput = document.querySelector('#named');
+let jobInput = document.querySelector('#job');
+
+let profileInfoName = document.querySelector('.profile__info_name');
+let profileInfoProfession = document.querySelector('.profile__info_profession');
+
 
 profileButton.addEventListener('click', (e)=>{
     e.preventDefault();
-    editForm.classList.add('open-form');
-    editForm.classList.add('modal');
+    popup.classList.add('popup__opened');
 })
 
-buttonClose.addEventListener('click', (e)=>{
+popupClose.addEventListener('click', (e)=>{
     e.preventDefault();
-    editForm.classList.remove('open-form');
+    popup.classList.remove('popup__opened');
+})
+
+popup.addEventListener('click', (e)=>{
+    e.preventDefault();
+    profileInfoName.textContent = namedInput.value;
+    profileInfoProfession.textContent = jobInput.value;
+})
+
+let elements = document.querySelector('.elements');
+let buttonLike = elements.querySelector('.elements__group_vector');
+
+function activeButton(){
+    if(buttonLike.classList.contains('elements__group_vector')){
+        buttonLike.classList.remove('elements__group_vector');
+        buttonLike.classList.add('elements__group_vector-active');
+    } else{
+        buttonLike.classList.remove('elements__group_vector-active');
+        buttonLike.classList.add('elements__group_vector');
+    }
+}
+
+buttonLike.addEventListener('click', (e)=>{
+    e.preventDefault();
+    activeButton();
 })
